84af40510d2b4b6800b03876edccb1aa3c865b05
Wairton Rebouças <wairtonjr@gmail.com>
2025-02-01 22:45:19 -0300

    move dump function to helper module

--- main.lua
+++ main.lua
@@ -1,19 +1,5 @@
 local v = require("validator")
-
--- https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
-
+local h = require("helper")
 
 -- print(v.parse_file(arg[1]))
 local res, offset = v.read_whitespace("  3", 1)
@@ -26,9 +12,9 @@ local res, offset = v.read_number("1234", 1)
 
 print("ta1")
 local res, offset = v.read_array("[1123]", 1)
-print(dump(res), offset)
+print(h.dump(res), offset)
 
 print("ta2")
 local res, offset = v.read_array("[1,2]", 1)
-print(dump(res), offset)
+print(h.dump(res), offset)
 
